3151bac843d3d9ea80045997cf0d9c32253d0dcc
Tomas <tomas@uow.edu.au>
2014-08-15 15:09:08 +1000

    Development SQL code for aggregate data and schema

---
+++ aggregates/aggregate_reports_schema.sql
@@ -0,0 +1,5 @@
+﻿CREATE TABLE rw_count_reports_unconfirmed (pkey bigserial, rw_pkey integer, count integer, start_time TIMESTAMP WITH TIME ZONE, end_time TIMESTAMP WITH TIME ZONE,
+CONSTRAINT rw_count_uc_pkey PRIMARY KEY (pkey));
+
+CREATE TABLE rw_count_reports_confirmed (pkey bigserial, rw_pkey integer, count integer, start_time TIMESTAMP WITH TIME ZONE, end_time TIMESTAMP WITH TIME ZONE,
+CONSTRAINT rw_count_pkey PRIMARY KEY (pkey));
