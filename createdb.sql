bba9df994975940bd786fe12d7763c4c97b63ec7
Mark Varley <mark@envision-it.co.uk>
2016-10-12 16:05:08 +0200

    Updated collation to en_US

--- createdb.sql
+++ createdb.sql
@@ -2,6 +2,6 @@
   WITH OWNER = postgres
        ENCODING = 'UTF8'
        TABLESPACE = pg_default
-       LC_COLLATE = 'en_AU.UTF-8'
-       LC_CTYPE = 'en_AU.UTF-8'
+       LC_COLLATE = 'en_US.UTF-8'
+       LC_CTYPE = 'en_US.UTF-8'
        CONNECTION LIMIT = -1;
