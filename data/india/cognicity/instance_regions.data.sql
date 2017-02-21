c612de27406b770e3753558c8ffb84daeafe1431
Tomas <tomasholderness@gmail.com>
2017-02-21 16:33:49 -0500

    Bounding box for Chennai

--- data/india/cognicity/instance_regions.data.sql
+++ data/india/cognicity/instance_regions.data.sql
@@ -1,1 +1,2 @@
-test
+-- Instance regions for RiskMap.in 2017
+INSERT INTO cognicity.instance_regions (code, name, the_geom) VALUES ('chn', 'Chennai', ST_GeomFromText('POLYGON((106.480 -6.733, 107.175 -6.733, 107.175 -5.880, 106.480 -5.880, 106.480 -6.733))',4326));
