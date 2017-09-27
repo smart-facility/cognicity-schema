ab2b7670b64dae122b8854f6456e0acf0ce57e90
Tomas <tomasholderness@gmail.com>
2017-09-27 14:54:48 -0400

    adding version test

---
+++ test/testVersion.js
@@ -0,0 +1,20 @@
+const test = require('unit.js');
+
+export default (db, version) => {
+  // Cards endpoint
+  describe('Schema version functionality', () => {
+
+    // Test
+    it ('Can get correct version', (done) => {
+
+      // Check the test data can be output with rem_get_flood function
+      let query = "SELECT * FROM cognicity.version()";
+      db.oneOrNone(query)
+        .then((data) => {
+          test.value(data.version).is(version);
+          done();
+        })
+        .catch((error) => test.fail(error));
+   });
+});
+}
