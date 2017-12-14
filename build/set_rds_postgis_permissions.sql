35c5878b470dab6fc88b36a622fb45d7295b012d
Tomas <tomasholderness@gmail.com>
2017-12-14 14:17:20 -0500

    updated rds function

--- build/set_rds_postgis_permissions.sql
+++ build/set_rds_postgis_permissions.sql
@@ -12,8 +12,8 @@ CREATE OR REPLACE FUNCTION set_rds_postgis_permissions()
       ALTER SCHEMA tiger owner to rds_superuser;
       ALTER SCHEMA topology owner to rds_superuser;
 
-      CREATE FUNCTION exec(text) returns text language plpgsql volatile AS $f$ BEGIN EXECUTE $1; RETURN $1; END; $f$;
-      SELECT exec('ALTER TABLE ' || quote_ident(s.nspname) || '.' || quote_ident(s.relname) || ' OWNER TO rds_superuser')
+      CREATE OR REPLACE FUNCTION exec(text) returns text language plpgsql volatile AS $f$ BEGIN EXECUTE $1; RETURN $1; END; $f$;
+      PERFORM exec('ALTER TABLE ' || quote_ident(s.nspname) || '.' || quote_ident(s.relname) || ' OWNER TO rds_superuser')
         FROM (
           SELECT nspname, relname
           FROM pg_class c JOIN pg_namespace n ON (c.relnamespace = n.oid)
